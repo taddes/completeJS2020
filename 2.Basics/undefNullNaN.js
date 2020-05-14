4226e4dfa70dd5959a75596db5eeeccb3d4f2f7e
Taddes Korris <taddes.korris@gmail.com>
2020-05-14 12:10:51 -0400

    control structures

---
+++ 2.Basics/undefNullNaN.js
@@ -0,0 +1,9 @@
+// undefined -> default value of uninitialized values. Don't assign any value to undefined
+let value; 
+
+// null -> no data, can be used to reset/clear a variable or initiate it as empty. Is an object
+let initialization = null;
+
+// NaN -> Not a number NOTE: A TYPE OF NUMBER
+let what = 'Hello' * 5;
+console.log(typeof value, typeof initialization, typeof what)
---
+++ 4.Control Structures/arrObjComparison.js
@@ -0,0 +1,10 @@
+let obj = {
+  name:'Pepper'
+};
+
+let obj2 = {
+  name:'Pepper'
+};
+
+// NOT THE SAME, because objects and arrays are seperate objects
+console.log(obj === obj2)
